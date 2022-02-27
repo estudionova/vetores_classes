c516ce1e419cf1a71dd575cfec1eb336e6f97383
Reinaldo Junior <rsljunior@live.com>
2022-02-27 17:19:16 -0300

    Finalizando

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,6 +12,24 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
+        }
+
+
+        double sum = 0.0;
+        for(int i = 0; i< vect.length; i++){
+            sum += vect[i].getPrice();
+        }
+
+        double avg = sum / vect.length;
+
+        System.out.printf("AVARAGE PRICE = %.2f%n", avg);
 
 
         sc.close();
